cef39cd3370acffdac32f43812ebe09917643645
Takada <tkdtkd0022gmail.com>
2017-11-26 17:26:03 +0900

    refactor

--- src/main/java/com/example/my/web/fw/launch/ServerLauncher.java
+++ src/main/java/com/example/my/web/fw/launch/ServerLauncher.java
@@ -1,7 +1,18 @@
 package com.example.my.web.fw.launch;
 
+import com.example.my.web.fw.server.Server;
+
 public class ServerLauncher {
   public static void main(String[] args) {
+    Server server;
+    if(args.length > 0) {
+      int port = Integer.valueOf(args[0]);
+      server = new Server(port);
+    } else {
+      server = new Server();
+    }
 
+    server.start();
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> server.stop()));
   }
 }
